748bf84d528af76d81207ad10a4757cb668682f4
Rompei <rompei0924@gmail.com>
2016-01-13 16:12:22 +0900

    Modify crawler.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
@@ -8,31 +9,101 @@ import (
 )
 
 // BaseURL is url for wikipedia.
-const BaseURL = "https://en.wikipedia.org/wiki/"
+const BaseURL = "https://en.wikipedia.org"
 
 type Language struct{
 	Name string `json:"name"`
 	URL string `json:"url"`
-	Ansestors []Language`json:"ansestors"`
+	Descendents []Language`json:"descendents"`
 }
 
-func crawl(l *Language, errCh chan error) {
+func (l Language)ShowLanguages(){
+	fmt.Println(l.Name)
+	for _,v := range l.Descendents{
+		v.ShowLanguages()
+	}
+}
+
+type CrawlChecker struct{
+	crawled []string `json:"crawled"`
+}
+
+func (c CrawlChecker)AddCrawled(name string){
+	c.crawled = append(c.crawled, name)
+}
+
+func (c CrawlChecker)GetCrawled() []string{
+	return c.crawled
+}
+
+func (c CrawlChecker)Len()int{
+	return len(c.crawled)
+}
+
+func crawl(l *Language, c *CrawlChecker) (*Language, error){
 	doc, err := goquery.NewDocument(l.URL)
-	if err != nil {
-		errCh <- err
-		return
+	if err != nil{
+		return l, err
 	}
 	var prevText string
 	doc.Find(".vevent").Find("tr").Each(func(i int, s *goquery.Selection) {
 		if prevText == "Influenced" {
 			s.Find("td").Find("a").Each(func(i int, s *goquery.Selection){
-				fmt.Println(s.Text())
-			})
-		}
+				href, ok  := s.Attr("href")
+				if !ok{
+					fmt.Println("This page was wrong.")
+					return
+				}
+				name, ok := s.Attr("title")
+				if !ok{
+					fmt.Println("This page was wrong.")
+					return
+				}
+				if(c.Len() ==0){
+					url := BaseURL + href
+					fmt.Printf("Start searching: %s, URL: %s\n", name, url)
+					desLang := &Language{
+						Name: name,
+						URL: url,
+					}
+					c.AddCrawled(name)
+					desLang, err = crawl(desLang, c)
+					if err !=nil{
+						fmt.Println(err)
+					}
+					l.Descendents = append(l.Descendents, *desLang)
+				}else{
+					isExist := false
+					for _,v := range c.GetCrawled(){
+						if v == name{
+							isExist = true
+						}
+					}
+					if isExist{
+						fmt.Printf("Stop searching: %s\n", name)
+						l.Descendents = append(l.Descendents, Language{Name:name})
+					}else{
+						url := BaseURL + href
+						fmt.Printf("Start searching: %s, URL: %s\n", name, url)
+						desLang := &Language{
+							Name: name,
+							URL: url,
+						}
+						c.AddCrawled(name)
+						desLang, err = crawl(desLang, c)
+						if err !=nil{
+							fmt.Println(err)
+						}
+						l.Descendents = append(l.Descendents, *desLang)
+					}
+				}
+			}
+		})
 		prevText = s.Find("th").Text()
 	})
+}
 
-	errCh <- nil
+return l, nil
 }
 
 func main() {
@@ -43,14 +114,17 @@ func main() {
 	}
 	url := flag.Args()[0]
 
-	errCh := make(chan error)
-	go crawl(&Language{
-		URL:url,
-		Name:"C",
-	}, errCh)
-
-	err := <-errCh
-	if err != nil {
-		fmt.Printf("%v", err)
+	l := &Language{
+		Name: "C",
+		URL: url,
 	}
+
+	lc := &CrawlChecker{}
+
+	language, err := crawl(l, lc)
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	language.ShowLanguages()
 }
