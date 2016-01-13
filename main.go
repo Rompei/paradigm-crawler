e42d1c23f3c8260bceeeb25d42fd193cf7cddced
Rompei <rompei0924@gmail.com>
2016-01-13 22:35:13 +0900

    Implemented.

--- main.go
+++ main.go
@@ -1,41 +1,68 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"os"
+	"strings"
 )
 
 // BaseURL is url for wikipedia.
 const BaseURL = "https://en.wikipedia.org"
 
+// Language is object of programing language.
 type Language struct {
 	Name        string     `json:"name"`
 	URL         string     `json:"url"`
 	Descendents []Language `json:"descendents"`
 }
 
-func (l Language) ShowLanguages() {
+// NewLanguage is constructor of Language.
+func NewLanguage(name, url string) *Language {
+	return &Language{
+		Name: name,
+		URL:  url,
+	}
+}
+
+// ShowLanguages shows languages with descendents.
+func (l *Language) ShowLanguages() {
 	fmt.Println(l.Name)
 	for _, v := range l.Descendents {
 		v.ShowLanguages()
 	}
 }
 
+// CrawlChecker is wrapper of crawled languages.
 type CrawlChecker struct {
 	crawled []string `json:"crawled"`
 }
 
-func (c CrawlChecker) AddCrawled(name string) {
+// NewCrawlChecker is constructot of CrawlChecker.
+func NewCrawlChecker() *CrawlChecker {
+	return &CrawlChecker{}
+}
+
+// AddCrawled adds crawled language to crawled.
+func (c *CrawlChecker) AddCrawled(name string) {
 	c.crawled = append(c.crawled, name)
 }
 
-func (c CrawlChecker) GetCrawled() []string {
+// ShowCrawled shows crawled languages.
+func (c *CrawlChecker) ShowCrawled() {
+	fmt.Println(strings.Join(c.crawled, ","))
+}
+
+// GetCrawled returns crawled languages.
+func (c *CrawlChecker) GetCrawled() []string {
 	return c.crawled
 }
 
-func (c CrawlChecker) Len() int {
+// Len returns length of crawled languages.
+func (c *CrawlChecker) Len() int {
 	return len(c.crawled)
 }
 
@@ -61,10 +88,7 @@ func crawl(l *Language, c *CrawlChecker) (*Language, error) {
 				if c.Len() == 0 {
 					url := BaseURL + href
 					fmt.Printf("Start searching: %s, URL: %s\n", name, url)
-					desLang := &Language{
-						Name: name,
-						URL:  url,
-					}
+					desLang := NewLanguage(name, url)
 					c.AddCrawled(name)
 					desLang, err = crawl(desLang, c)
 					if err != nil {
@@ -78,16 +102,10 @@ func crawl(l *Language, c *CrawlChecker) (*Language, error) {
 							isExist = true
 						}
 					}
-					if isExist {
-						fmt.Printf("Stop searching: %s\n", name)
-						l.Descendents = append(l.Descendents, Language{Name: name})
-					} else {
+					if !isExist {
 						url := BaseURL + href
 						fmt.Printf("Start searching: %s, URL: %s\n", name, url)
-						desLang := &Language{
-							Name: name,
-							URL:  url,
-						}
+						desLang := NewLanguage(name, url)
 						c.AddCrawled(name)
 						desLang, err = crawl(desLang, c)
 						if err != nil {
@@ -104,25 +122,39 @@ func crawl(l *Language, c *CrawlChecker) (*Language, error) {
 	return l, nil
 }
 
+func dump(languageTree *Language, fname string) error {
+	b, err := json.Marshal(languageTree)
+	if err != nil {
+		return err
+	}
+
+	var out bytes.Buffer
+	json.Indent(&out, b, "", "\t")
+
+	file, err := os.OpenFile(fname, os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+	file.Truncate(0)
+	out.WriteTo(file)
+	return nil
+}
+
 func main() {
 	flag.Parse()
-	if flag.NArg() != 1 {
-		fmt.Println("Error")
+	if flag.NArg() != 2 {
+		fmt.Println("not enough argument")
 		os.Exit(1)
 	}
-	url := flag.Args()[0]
-
-	l := &Language{
-		Name: "C",
-		URL:  url,
-	}
-
-	lc := &CrawlChecker{}
-
-	language, err := crawl(l, lc)
+	name := flag.Args()[0]
+	url := flag.Args()[1]
+	l := NewLanguage(name, url)
+	cc := NewCrawlChecker()
+	language, err := crawl(l, cc)
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	language.ShowLanguages()
+	dump(language, "output.txt")
 }
